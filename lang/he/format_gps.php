687ccc8968a295c67f3a2d2b000695d064a7876c
Nadav Kavalerchik <nadavkav@gmail.com>
2015-06-26 00:08:05 +0300

    Hebrew translation
    Hello Juergen,
    Please see if you can add the above Hebrew translation.
    Kindly,
    Nadav.

---
+++ lang/he/format_gps.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of the GPS Free course format for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_gps', language 'he', branch 'MOODLE_28_STABLE'
+ *
+ * @package    format_gps
+ * @copyright  kamedia - 2014 Barry Oosthuizen
+ * @author     Barry Oosthuizen http://www.kamedia.de
+ * @translator Nadav Kavalerchik nadavkav@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['active'] = 'הגבילו צפיה ביחידת הוראה זו על בסיס הימצאות במקום גאוגרפי מסויים';
+$string['address'] = 'תאור המקום';
+$string['continue'] = 'הקליקו כאן להמשך';
+$string['currentsection'] = 'יחידת הוראה זו';
+$string['editgeolocation'] = 'הגדרות מיקום גאוגרפי';
+$string['editsection_geo'] = 'עריכת הגדרות מיקום גאוגרפי עבור יחידת הוראה זו';
+$string['editsection_geo_heading'] = 'הגדרות מיקום גאוגרפי';
+$string['errorhelp'] = 'Form validation error'; 
+$string['errorhelp_help'] = 'Latitude and Longitude are required fields if you restrict the topic to a location.'; 
+$string['expandmap'] = 'היסתמשו במפה האינטראקטיבית';
+$string['feet'] = 'רגל';
+$string['googlemapkey3error'] = 'You need to specify a Google maps 3 API key for your site before you can use Google maps'; 
+$string['gpshelp_help'] = 'הגדרות מיקום גאוגרפי של יחידת הוראה';
+$string['gpshelp'] = "With GPS settings you can restrict the visability of a topic ".
+        "to only shown if a learner has a GPS enabled device, has allowed their GPS location ".
+        "to be shared with Moodle and if he or she is at the location you specify here. ".
+        "Hint: GPS settings are deleted if you change the course format to another format.";
+$string['gps:editsection_geo'] = 'עריכת הגדרות מיקום גאוגרפי';
+$string['gps_restricted'] = 'יחידת הוראה זו זמינה עבור מי שנמצא במיקום גאוגרפי מסויים';
+$string['gpsrestricted'] = 'זמין רק כאשר נמצאים במיקום הגאוגרפי המדוייק';
+$string['hidefromothers'] = 'הסתרת יחידת הוראה';
+$string['kilometers'] = 'קילומטרים';
+$string['latitude'] = 'קו-הרוחב של המיקום';
+$string['longitude'] = 'קו-האורך של המיקום';
+$string['loadinggps'] = 'מעדכן מידע גאוגרפי - GPS';
+$string['mapinfo'] = "הקילוק על המפה לציון מיקום, ואז הקליקו על הסמן הירוק כדי להשתמש במידע הגאוגרפי של המיקום";
+$string['meters'] = 'מטרים';
+$string['miles'] = 'מיילים';
+$string['notallowed'] = 'עדכנו את המיקום שלכם כדי להציג את יחידת ההוראה';
+$string['page-course-view-gps'] = 'Any course main page in GPS Format';
+$string['page-course-view-gps-x'] = 'Any course page in GPS Format';
+$string['pluginname'] = 'למידה מבוססת מקום';
+$string['pluginname_help'] = 'The GPS Format Free allows you to restrict a topic to a specific GPS location. The student has to be inside the specified location radius to get access to the topic.';
+$string['radius'] = 'שימוש ברדיוס, לא אפשרי';
+$string['restricted'] = 'חסום ומותנה על ידי הימצאות במיקום גאוגרפי מסויים';
+$string['section0name'] = 'כללי';
+$string['sectionname'] = 'יחידת הוראה';
+$string['setgooglemapkey3'] = 'Click here to set your Google Maps 3 API key now'; 
+$string['sharelocation'] = 'This course can only be accessed if you agree to share your GPS location';
+$string['showfromothers'] = 'תצוגת יידת הוראה';
+$string['unrestricted'] = 'לא מוגבל על ידי מיקום גאוגרפי';
+$string['updatepage'] = 'עדכון עמוד הקורס';
+$string['updateposition'] = 'עדכון המיקום שלי';
+$string['validationerror'] = 'There was a problem with the values you entered in the form.  Please correct any errors and resubmit the form.';
+$string['yards'] = 'יארדים';
